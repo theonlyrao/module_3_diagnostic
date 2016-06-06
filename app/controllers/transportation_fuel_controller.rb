33a83e1783330cfee200af6ea979b4ea0b3fc0fb
Ashwin Rao <theonlyrao@gmail.com>
2016-06-06 10:12:33 -0600

    getting fuel stations to render properly

--- app/controllers/transportation_fuel_controller.rb
+++ app/controllers/transportation_fuel_controller.rb
@@ -2,7 +2,7 @@ class TransportationFuelController < ApplicationController
   def index
     zip = params["zip"]
     conn = Faraday.new("https://developer.nrel.gov")
-    response = conn.get "/api/alt-fuel-stations/v1.json?api_key=#{Pusher.nrel_api_key}&zip=#{zip}&limit=10"
-    @stations = JSON.parse(response.body)
+    response = conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{Pusher.nrel_api_key}&location=#{zip}"
+    @stations = JSON.parse(response.body["fuel_stations"])
   end
 end

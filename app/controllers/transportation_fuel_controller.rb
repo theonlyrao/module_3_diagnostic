55ca02a32160d0d657f1f00a9fb50bac5e1858c9
Ashwin Rao <theonlyrao@gmail.com>
2016-06-06 10:31:51 -0600

    Kind of finish 1

--- app/controllers/transportation_fuel_controller.rb
+++ app/controllers/transportation_fuel_controller.rb
@@ -2,7 +2,7 @@ class TransportationFuelController < ApplicationController
   def index
     zip = params["zip"]
     conn = Faraday.new("https://developer.nrel.gov")
-    response = conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{Pusher.nrel_api_key}&location=#{zip}&raidus=6&fuel_type=ELEC,LPG"
+    response = conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{Pusher.nrel_api_key}&location=#{zip}&raidus=6&fuel_type=ELEC,LPG&limit=10"
     parsed = JSON.parse(response.body)
     @stations = parsed["fuel_stations"]
   end
